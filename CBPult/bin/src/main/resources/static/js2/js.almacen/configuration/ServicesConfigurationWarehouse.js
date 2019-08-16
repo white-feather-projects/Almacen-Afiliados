43960c018b4cf62040b467b3238c9c0302c98617
DefaultJARR <rrja98@gmail.com>
2019-08-16 15:45:55 -0500

    # Cambios Commit: Comentarios de Servicios Almacén
    1. Algunos comentarios de los servicios de Almacén
    
    Nota:
    - Terminar de revisar y enviar incidencias a Rosa y el Grupo

---
+++ CBPult/bin/src/main/resources/static/js2/js.almacen/configuration/ServicesConfigurationWarehouse.js
@@ -0,0 +1,237 @@
+function listarAlmacenesServ(){
+		
+    $('#configuracionAlmacen').DataTable( {
+	    
+    	sort:true,
+    	destroy: true,
+    	searching: true,
+      	
+    	language: {
+    		url: '../js2/Spanish.json'
+        },
+        ajax: {
+        	url:"/CBPult/Almacen/listaAlmacenes",
+            dataSrc: '',
+        	
+        },
+      
+        columns: [ 
+      
+    	   {
+                "data": "idWarehouse", // can be null or undefined
+                "class": "uno",
+                "defaultContent": "",
+                "render": function ( data ) {
+            		return '<center>'+data+'</center>';
+              	}
+            },
+            {
+                "data": "warehouseName", // can be null or undefined
+                "class": "dos",
+                "defaultContent": "",
+                "render": function ( data ) {
+            		return '<center>'+data+'</center>';
+              	}
+            },
+            {
+                "data": "direccion", // can be null or undefined
+                "class": "tres",
+                "defaultContent": "",
+                "render": function ( data ) {
+            		return '<center>'+data+'</center>';
+              	}
+            },
+            {
+                "data": "tipoAlmacenId", // can be null or undefined
+                "class": "cuatro",
+                "defaultContent": "",
+                "render": function ( data ) {
+            		return '<center>'+data+'</center>';
+              	}
+            },
+            {
+                "data": "gerenteSucursal", // can be null or undefined
+                "class": "cinco",
+                "defaultContent": "",
+                "render": function ( data ) {
+            		return '<center>'+data+'</center>';
+              	}
+            },
+            {
+                "data": "idWarehouse", // can be null or undefined
+                "class": "seis",
+                "defaultContent": "",
+                "render": function ( data ) {
+                	return '<center><a href="/CBPult/Almacen/almacen_nuevo-editar/'+data+'" title="Editar solicitud de orden de compra"><input type="hidden" id="edit" value="'+data+'"><i class="fa fa-edit" agregaryasignar" style="font-size:30px"></i></a></center>';
+              	}
+            }
+        ]
+        
+    });
+
+}
+
+function cargarDatosAlmacenServ() {
+	var url = window.location.pathname;
+	var idWarehouse = url.substring(url.lastIndexOf('/') + 1);
+	
+	var url3='/CBPult/Almacen/consultarAlmacenPorAlmacenId';
+	var datos = {
+		"idAlmacen": idWarehouse			
+	}
+
+	$.ajax({
+	    url:url3,
+	    dataType: 'json',
+	    contentType:'application/json',
+	    data:JSON.stringify(datos),
+	    type: 'POST',
+	    success: function(resp){
+		    console.log(resp);		   
+		    console.log(resp.return.idWarehouse);
+		    
+		    document.getElementById('txtCodigo_almacen').value = resp.return.idWarehouse;
+			$("#cboxTipo_almacen option[value="+resp.return.tipoAlmacenId+"]").attr("selected",true);
+			
+		    document.getElementById('txtName_almacen').value = resp.return.warehouseName;
+		    
+		    document.getElementById('txtUbicacion_almacen').value = resp.return.direccion;
+		    $("#cboxEncargado_almacen option[value="+resp.return.gerenteSucursal+"]").attr("selected",true);
+		    document.getElementById('numeroAlmacen').value = resp.return.warehouseNumber;
+		},
+	    error: function(e){
+	    	console.log("errro:"+e);
+		}
+	}); 
+}
+
+function modificarAlmacenServ(){
+	
+	var numeroAlmacen = $("#numeroAlmacen").val();
+	var nombreAlmacen = $("#txtName_almacen").val();
+	var tipoAlmacen = $("#cboxTipo_almacen").val();
+	var direccion =  $("#txtUbicacion_almacen").val();
+	var gerente =  $("#cboxEncargado_almacen").val();
+	  
+	var contenido = {
+		"numeroAlmacen": numeroAlmacen,
+        "nombreAlmacen": nombreAlmacen,
+        "tipoAlmacen": tipoAlmacen,
+        "direccion": direccion,
+        "gerenteSucursal": gerente,		           
+	}
+	
+	$.ajax({
+		headers: { 
+			'Accept': 'application/json',
+	        'Content-Type': 'application/json' 
+	    },
+   
+        type: "POST",	  
+        url: "/CBPult/Almacen/modificarAlmacen",
+        data: JSON.stringify(contenido),
+        dataType: "json",
+        success: function(data){
+        	if(data.return.descripcion=='OK'){			
+        		swal({
+        			title: 'Almacen modificado con exito',
+		    	    text: 'Redirigiendo...',
+		    	    icon: 'Exitoso',
+		    	    timer: 1000,
+		    	    buttons: false,
+        		},
+    		    function() {
+        			location.href = "/CBPult/Almacen/configuration_almacen";
+        		})		    	
+        	}
+		    else{
+		    	swal("Solicitud de orden de compra en carga no Exitosa!!");	          
+		    }
+        }	   
+	}); 
+}
+
+// Servicios para el Wizard
+function listarAlmacenesRelServ(){
+	
+    $('#simpletablerel').DataTable( {
+	    
+    	sort:true,
+    	destroy: true,
+    	searching: true,
+    	paging: false, // le quito el paging porque no deja cargar los switches de las otras paginas
+      	
+    	language: {
+    		url: '../js2/Spanish.json'
+        },
+        ajax: {
+        	url:"/CBPult/Almacen/listaAlmacenes",
+            dataSrc: ''
+        	
+        },       
+        columns: [ 
+        	
+           {
+                "class": "uno",
+                "defaultContent": "",
+                "render": function ( data ) {
+                	return '<center><input type="checkbox" class="js-switch-blue" checked="" data-switchery="true" style="display: none;"></center>';
+                }
+           },
+    	   {
+                "data": "idWarehouse", // can be null or undefined
+                "class": "dos",
+                "defaultContent": "",
+                "render": function ( data ) {
+            		return '<center>'+data+'</center>';
+              	}
+            },
+            {
+                "data": "tipoAlmacenId", // can be null or undefined
+                "class": "cinco",
+                "defaultContent": "",
+                "render": function ( data ) {
+            		return '<center>'+data+'</center>';
+              	}
+            },
+            {
+                "data": "warehouseName", // can be null or undefined
+                "class": "tres",
+                "defaultContent": "",
+                "render": function ( data ) {
+            		return '<center>'+data+'</center>';
+              	}
+            },            
+            {
+                "data": "direccion", // can be null or undefined
+                "class": "cuatro",
+                "defaultContent": "",
+                "render": function ( data ) {
+            		return '<center>'+data+'</center>';
+              	}
+            },            
+            {
+                "data": "gerenteSucursal", // can be null or undefined
+                "class": "seis",
+                "defaultContent": "",
+                "render": function ( data ) {
+            		return '<center>'+data+'</center>';
+              	}
+            }            
+        ],
+        // ejecuta una funcion Despues de Cargar los Registros de la Tabla
+        // lo uso para cargar la clase de los Switches
+        initComplete: function( settings, json){
+            console.log(json);
+            alert("tabla terminada");
+            //Cargar Switches
+			Array.prototype.forEach.call($('.js-switch-blue'), (item, i)=>{
+				var actual_switch = new Switchery(item, {
+					color: '#17a2b8'
+				});
+			});
+        },
+        
+    });
+    
+}
--- CBPult/bin/src/main/resources/static/js2/js.almacen/configuration/almacen_nuevo-wizzard_validations.js
+++ CBPult/bin/src/main/resources/static/js2/js.almacen/configuration/almacen_nuevo-wizzard_validations.js
@@ -1,9 +1,11 @@
 var data_almacen; // para capturar la toda la info del modulo (Almacen, Zonas, Relaciones)
-var data_zonas; // se usa para capturar las Zonas i se incrusta en "data_almacen" en el atributo "zonasAlmacen"
-var contador_switches = 0;
+var data_zonas;// se usa para capturar las Zonas i se incrusta en "data_almacen" en el atributo "zonasAlmacen"
 
 var txtDescAlmacen = $('#txtDesc_almacen');
 var txtUbicacionAlmacen = $('#txtUbicacion_almacen');
+
+var contenedor_relZonas = $('#relaciones_zonas');
+var listar = 0;
 
 $(document).ready(function(){
 
@@ -52,17 +54,116 @@ $('#demo').steps({
 					txtUbicacionAlmacen.css("border" , "1px solid red");
 				}
 				///
+				
+				// Validacion Encargado
+				var Encargado = $('#cboxEncargado_almacen').val(); // lo necesito porque el servicio de Crear me pide el IdTipo Almacen, mas no el Tipo en si
+				var idEncargado;
+				if(Encargado === 'Elon Musk'){
+					idEncargado = 1;
+				}
+				else if(Encargado === 'Einstein'){
+					idEncargado = 2;
+				}
+				else if(Encargado === 'Jack Ma'){
+					idEncargado = 3;
+				}
+				else{
+					console.log("Encargado no Existente");
+					idEncargado = null;
+				}
+				// Validacion Encargado
+				
+				// Validacion Tipo Almacen
+				var tipoAlmacen = $('#cboxTipo_almacen').val(); // lo necesito porque el servicio de Crear me pide el IdTipo Almacen, mas no el Tipo en si
+				var idTipoAlmacen;
+				if(tipoAlmacen === 'Almacén'){
+					idTipoAlmacen = 1;
+				}
+				else if(tipoAlmacen === 'Laboratorio'){
+					idTipoAlmacen = 2;
+				}
+				else if(tipoAlmacen === 'Sucursal'){
+					idTipoAlmacen = 3;
+				}
+				else{
+					console.log("Tipo de Almacén no Existente");
+					idTipoAlmacen = null;
+				}
+				// Validacion Tipo Almacen
+				
+				
+				// Validacion y Creacion Id Almacen
+				var warehouseText;
+				var IdAlmacen = $('#txtCodigo_almacen').val();
+				if(idTipoAlmacen == 1){
+					warehouseText = "Almacen-"+IdAlmacen;
+				}		
+				else if(idTipoAlmacen == 2){
+					warehouseText = "Laboratorio-"+IdAlmacen;
+				}
+				else if(idTipoAlmacen == 3){
+					warehouseText = "Sucursal-"+IdAlmacen;
+				}
+				else{
+					console.log("No se puede Enviar Id, Tipo de Almacén no Existente");
+					warehouseText = null;
+				}
+				// Validacion y Creacion Id Almacen
+				
 				if(validated == 2){
+					
+					// Creacion del Almacén
+					
 					data_almacen = {
-							'idAlmacen': $('#txtCodigo_almacen').val(),
-							'tipoAlmacen': $('#cboxTipo_almacen').val(),
-							'descripcionAlmacen': $('#txtDesc_almacen').val(),
-							'encargadoAlmacen': $('#cboxEncargado_almacen').val(),
-							'ubicacionAlmacen': $('#txtUbicacion_almacen').val(),
-							'zonasAlmacen': {},
-							'relacionesAlmacen': {}
+						"direccion": $('#txtUbicacion_almacen').val(),
+					    "gerenteSucursal": idEncargado,
+					    "officeId": 1,
+					    "productId": 1,
+					    "tipoAlmacenId": idTipoAlmacen,
+					    "warehouseName": $('#txtDesc_almacen').val(),
+					    "warehouseNumber": warehouseText
 					};
-					console.log(data_almacen);
+					/* Consumo 
+					$.ajax({
+ 		               type: "POST",
+ 		               url: '/CBPult/Afiliacion/crearAlmacen',
+ 		               contentType: "application/json",
+ 		               dataType: "json",
+ 		               data: JSON.stringify(data_almacen),
+ 		               success: processSuccess,
+ 		               error: processError
+	 		        });
+					function processSuccess(data, status, req) {
+	 		            //alert(req.responseText + " " + status);
+	 		       		console.log(data);
+	 		       		
+		 		       	if(data.return.descripcion === "FAIL"){
+	 		           		swal("Error al Crear Almacén", data.return.descripcion);
+	 		           		return false;	 		           		
+	 		           	}else if(data.return.descripcion === "OK"){
+		 		           	document.getElementById("txtUbicacion_almacen").disabled = true;
+		 		            document.getElementById("cboxEncargado_almacen").disabled = true;
+		 		            document.getElementById("cboxTipo_almacen").disabled = true;
+		 		            document.getElementById("txtDesc_almacen").disabled = true;
+		 		            document.getElementById("txtCodigo_almacen").disabled = true;
+		 		           	
+		 		           	swal("Almacén Creado", data.return.descripcion);
+	 		           		return true;	 		           		
+	 		           	}else if(data.return.descripcion === "Almacen ya existe"){
+	 		           		valid = false;
+	 		           		return valid;	 		           		
+	 		           	}
+ 		       		}
+ 		       	
+ 		       		function processError(data, status, req) {
+	 		            //alert(req.responseText + " " + status);
+	 		       		swal("Error al contactar el servicio", data);
+	 		           	valid = false;
+	 		           	return valid;
+ 		       		}*/
+					// Creacion del Almacén
+					
+					//console.log(data_almacen);
 					//swal("Valido");
 					
 				}else if(validated != 2){
@@ -77,36 +178,105 @@ $('#demo').steps({
 		
 		
 		// tab Zonas
-		/// Las validaciones de la parte de Zonas está en "almacen_nuevo-wizzard_funcionalidad.js"
+		/// Las valla parte de Zonas está en "almacen_nuevo-wizzard_funcionalidad.js"
 		if(currentIndex === 1){
+			
+			
 			
 			if(stepDirection === 'forward'){
 				
-				// Capturar los ids de las Zonas
-				var zonas = $('.zona_zonas');
-				console.log(zonas.length);
-				for(i=0; i<zonas.length; i++){
-					var zona_id =zonas[i].getAttribute('id');
-					var id_actual = zona_id.substring(4,5); 
-					console.log(id_actual);					
-				}
-				// Capturar los ids de las Zonas
-				if(contador_switches == 0){
-					recorrerswitch(); // para recargar los switches de las tablas en relaciones de Zonas
-					contador_switches++;
-				}
+				// Armando el Json de listZonas		
+				console.log("Zonas que pasan a Relaciones", zonaListTemp);				
+				data_zonas = zonaListTemp.map(function(zona, i){	
+					var estanterias = $('#simpletable'+zona.zona.id+' tbody tr');
+					var estanterias_json = [];
+					
+					for (var i = 0; i < estanterias.length; i++) {
+						var modulo, nivel;
+						for (var j = 0; j < estanterias[i].cells.length; j++) {							
+							if(j == 1){
+								modulo = estanterias[i].cells[j].innerHTML;
+							}
+							else if(j == 2){
+								nivel = estanterias[i].cells[j].innerHTML;
+							}							
+						}
+						estanterias_json.push([modulo, nivel]);
+					}
+					
+					return {
+						"id": zona.zona.id,
+						"tipoZona": $('#cboxTipo_zona'+zona.zona.id).val(),
+						"descripcionZona": $('#txtDescripcion_zona'+zona.zona.id).val(),
+						"encargadoZona": $('#cboxEncargado_zona'+zona.zona.id).val(),
+						"estanteriasZona": estanterias_json
+					}
+				});				
+				console.log("Pasan a Relaciones: ", data_zonas);
 				
-				var txts_num = $('.solo_num');
-				console.log(txts_num);
 			}			
 			
 		}		
-		// tab Zonas	
+		// tab Zonas
+		
+		if(currentIndex === 2){
+			
+			// para no cargar a cada rato la Tabla de Relaciones
+			if(listar == 0){
+				listarAlmacenesRelServ();
+				listar++;
+			}
+			
+			for (var i = 0; i < data_zonas.length; i++) {
+				console.log("Zona Actual", data_zonas[i]);
+				console.log('Relaciones de Zona '+i);
+				
+				// Llenado de los campos de las Zonas en Relaciones
+				$('#cboxTipo_zonarel'+data_zonas[i].id).val(data_zonas[i].tipoZona);
+				$('#txtDescripcion_zonarel'+data_zonas[i].id).val(data_zonas[i].descripcionZona);
+				$('#cboxEncargado_zonarel'+data_zonas[i].id).val(data_zonas[i].encargadoZona);
+				// Llenado de los campos de las Zonas en Relaciones
+				
+				for (var j = 0; j < data_zonas.length; j++) {
+					
+					if(j != i){
+						console.log(data_zonas[j]);
+						$('#tbodyrel_'+data_zonas[i].id).append(`
+							<tr>
+								<td style="text-align:center">
+									<div class="row" style="display: inline-block">
+		                                <div class="col-sm-4"><input type="checkbox" class="js-switch-blue_rel" checked="" data-switchery="true" style="display: none;"></div>
+		                            </div>
+								</td>							
+								<td>`+data_zonas[j].tipoZona+`</td>
+								<td>`+data_zonas[j].descripcionZona+`</td>
+								<td>`+data_zonas[j].encargadoZona+`</td>
+							</tr>
+						`);
+					}					
+					
+				}				
+				
+			}			
+			// Cargar Switches de las Tablas de Relación de las Zonas
+			Array.prototype.forEach.call($('.js-switch-blue_rel'), (item, i)=>{
+				var actual_switch = new Switchery(item, {
+					color: '#17a2b8'
+				});
+			});
+			
+			if(stepDirection === 'backward'){
+				// para limpiar los registros de las Tablas en Relaciones
+				for (var i = 0; i < data_zonas.length; i++) {					
+					$('#tbodyrel_'+data_zonas[i].id+' *').remove();					
+				}				
+			}
+			
+		}
     	
 		return true;        
 	},
 	onFinish: function () {
-		//alert('Wizard Completed');
 		location.href = "/CBPult/Almacen/configuration_almacen";
 	}
 
